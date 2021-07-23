c3f8185cd57aa3b7b576247052d9b99fd075cca8
Sarah <segglestn@gmail.com>
2021-07-23 14:37:33 +0100

    Rubocop config and fixes

--- app/helpers/collection_helper.rb
+++ app/helpers/collection_helper.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 module CollectionHelper
-
   def human_keys(keys)
     keys.map do |key|
-     [key.humanize, key]
+      [key.humanize, key]
     end
   end
 end
--- app/services/records_controller_service.rb
+++ app/services/records_controller_service.rb
@@ -2,15 +2,18 @@
 
 module RecordsControllerService
   module_function
+
   include Rails.application.routes.url_helpers
 
   def records_medicines(params)
     params.each do |param|
-      next if param.first == 'record_id'
+      next if param.first == "record_id"
+
       values = param.last
       next if values[:score].blank?
 
-      RecordsMedicine.create(record_id: params[:record_id], score: values[:score].to_i, medicine_id: values[:medicine_id], side_effects: values[:side_effects])
+      RecordsMedicine.create(record_id: params[:record_id], score: values[:score].to_i,
+                             medicine_id: values[:medicine_id], side_effects: values[:side_effects])
     end
 
     "/records_medicines/new?record_id=#{params[:record_id]}"
@@ -18,11 +21,13 @@ module RecordsControllerService
 
   def records_activities(params)
     params.each do |param|
-      next if param.first == 'record_id'
+      next if param.first == "record_id"
+
       values = param.last
       next if values[:score].blank?
 
-      RecordsActivity.create(record_id: params[:record_id], score: values[:score].to_i, activity_id: values[:activity_id])
+      RecordsActivity.create(record_id: params[:record_id], score: values[:score].to_i,
+                             activity_id: values[:activity_id])
     end
 
     "/records_medicines/new?record_id=#{params[:record_id]}"
